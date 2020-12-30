b6dea33dd1f2c2964557cf4533ff1877d7797403
Manohar Govind <manohar.govind@tcs.com>
2020-12-30 18:47:21 +0530

    msg

--- ProductService/src/main/java/com/practice/springcloud/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.practice.springcloud.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.practice.springcloud.model.product;
-
-public interface ProductRepo extends JpaRepository<product, Long> {
-
-}
